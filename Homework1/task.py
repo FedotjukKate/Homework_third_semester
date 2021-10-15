66c61a6458063755654bbdd4f59f519b553c4e1b
FedotjukKate <fedotjuk@icloud.com>
2021-10-15 20:05:06 +0500

    import class Ui_MainWindow from window.py

--- Homework1/task.py
+++ Homework1/task.py
@@ -1,8 +1,9 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from Homework1.window import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
