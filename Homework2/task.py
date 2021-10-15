aecefb97f11cb9fbb2293cf883e07cf4908d074c
FedotjukKate <fedotjuk@icloud.com>
2021-10-15 20:16:21 +0500

    create base

---
+++ Homework2/task.py
@@ -0,0 +1,31 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from HomeWork1.* import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.initUi()
+
+    def initUi(self):
+        pass
+
+sys._excepthook = sys.excepthook
+
+
+def exception_hook(exctype, value, traceback):
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+
+sys.excepthook = exception_hook
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec())
