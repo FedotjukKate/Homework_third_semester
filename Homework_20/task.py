db506e771c35d919118843366c08fa44fe24b6c4
FedotjukKate <fedotjuk@icloud.com>
2021-12-03 18:43:10 +0500

    homework_20

---
+++ Homework_20/task.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+file = input("Введите название файла: ")
+connection = sqlite3.connect(file)
+cursor = connection.cursor()
+
+result = cursor.execute("""
+SELECT title FROM films
+WHERE title LIKE "%Астерикс%" AND title NOT LIKE "%Обеликс%"
+""").fetchall()
+result = list(map(lambda x: str(x)[1:(len(str(x)) - 2):], result))
+
+print("\n".join(result))
